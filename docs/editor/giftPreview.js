37d262ff69238464307b676f41129d3228af0b59
Cris Fuhrman <fuhrmanator+git@gmail.com>
2018-07-17 22:49:40 -0400

    Matching works with lines in SVG

--- docs/editor/giftPreview.js
+++ docs/editor/giftPreview.js
@@ -7,42 +7,43 @@ function giftPreviewHTML(qArray, theDiv) {
     for (var i=0; i<qArray.length; i++) {
         // console.log("i = " + i + " and qArray[i] is '" + qArray[i] );
         q = qArray[i];
+        html += '<div id="question_' + i + '">';
         // console.log("q.type is '" + q.type );
         switch (q.type) {
             case "MC":
                 html += makeTitle("Multiple choice", q.title);
                 html +="<p>" + applyFormat(q.stem) + "</p>";
                 html += formatAnswers(q.choices);
-                html += formatFeedback(q.globalFeedback);
+                html += formatFeedback(q.globalFeedback) + "</div>";
                 theDiv.append(html); html = "";
                 break;
             case "Short":
                 html += makeTitle("Short answer", q.title);
                 html +="<p>" + applyFormat(q.stem) + "</p>";
                 html += formatAnswers(q.choices);
-                html += formatFeedback(q.globalFeedback);
+                html += formatFeedback(q.globalFeedback) + "</div>";
                 theDiv.append(html); html = "";
                 break;
             case "Essay":
                 html += makeTitle("Essay", q.title);
                 html +="<p>" + applyFormat(q.stem) + "</p>";
-                html += formatFeedback(q.globalFeedback);
+                html += formatFeedback(q.globalFeedback) + "</div>";
                 theDiv.append(html); html = "";
                 // html += formatAnswers(q.choices);
                 break;
            case "TF":
                 html += makeTitle("True/False", q.title);
                 html +="<p>" + "<em>(" + (q.isTrue ? "True" : "False") + ")</em> " + applyFormat(q.stem) + "</p>";
-                html += formatFeedback(q.globalFeedback);
+                html += formatFeedback(q.globalFeedback) + "</div>";
                 theDiv.append(html); html = "";
                 // html += formatAnswers(q.choices);
                 break;
             case "Matching":
                 html += makeTitle("Matching", q.title);
                 html +="<p>" + applyFormat(q.stem) + "</p>";
-                theDiv.append(html); html = "";
-                formatMatchingAnswers(q, theDiv);
-                html += formatFeedback(q.globalFeedback);
+//                theDiv.append(html); html = "";
+                formatMatchingAnswers(q, theDiv, i, html);
+                html = formatFeedback(q.globalFeedback) + "</div>";
                 theDiv.append(html); html = "";
                 break;
             // for case "Matching": see https://stackoverflow.com/a/35493737/1168342
@@ -73,16 +74,16 @@ function formatAnswers(choices) {
     return html;
 }
 
-function formatMatchingAnswers(q, theDiv) {
+function formatMatchingAnswers(q, theDiv, qNum, html) {
     var rightSideChoices = new Set();
-    var html ='';
+//    var html ='';
     var htmlLines = '';
     for (var i=0; i<q.matchPairs.length; i++) {
         htmlLines += '<line id="line' + i + '" stroke="red"/>';
     }
-    html += '<svg id="theSVG" style="position: absolute; z-index: -1;" width="100%" height="100%" z-index="-1">' + htmlLines + '</svg>';
+    html += '<svg class="theSVG" style="position: absolute; z-index: -1;" width="100%" height="100%" z-index="-1">' + htmlLines + '</svg>';
 
-    html +='<div class="row"><div class="col-md-5">';  // left side
+    html +='<div class="row"><div class="col-md-6">';  // left side
     for (var i=0; i<q.matchPairs.length; i++) {
         var pair = q.matchPairs[i];
         html += '<div id="left' + i + '" style="background:#ddddff">';
@@ -94,7 +95,7 @@ function formatMatchingAnswers(q, theDiv) {
     console.log('Found rightSideChoices: ' + rightSideChoices.size);
     html += '</div>'
 
-    html += '<div class="col-md-5 col-md-offset-6">'; // right side
+    html += '<div class="col-md-6">'; // right side
     for (var it = rightSideChoices.values(), val= null; val=it.next().value;) {
         //console.log('answer: ' + val);
         html += '<div id="right_' + val.replace(/\s/g, '_') + '" style="background:#ddddff">';
@@ -106,21 +107,22 @@ function formatMatchingAnswers(q, theDiv) {
     // make the SVG for lines, see https://stackoverflow.com/a/35493737/1168342
     theDiv.append(html); html = "";  // force the HTML into the DOM
 
-    // set the lines up 
+    // set the lines up - search only inside this question's div (theDiv) 
     for (var i=0; i<q.matchPairs.length; i++) {
+        var qDiv = $("#question_" + qNum);
         var pair = q.matchPairs[i];
-        var line = $("#line" + i);
-        var leftDiv = $("#left" + i); 
-        var rightDiv = $("#right_" + pair.subanswer.replace(/\s/g, '_'));
+        var line = qDiv.find("#line" + i); console.log("line: " + line);
+        var leftDiv = qDiv.find("#left" + i);  console.log("leftDiv: " + leftDiv);
+        var rightDiv = qDiv.find("#right_" + pair.subanswer.replace(/\s/g, '_')); console.log("rightDiv: " + rightDiv);
         var pos1 = leftDiv.offset();
         var pos2 = rightDiv.offset();
-        var svg = $("#theSVG").offset();
+        var svg = qDiv.find(".theSVG").offset();
         line
           .attr('x1', pos1.left - svg.left + leftDiv.width())
           .attr('y1', pos1.top + leftDiv.height()/2 - svg.top)
           .attr('x2', pos2.left - svg.left)
           .attr('y2', pos2.top + rightDiv.height()/2 - svg.top);
-        $("#theSVG").attr('height', pos1.top + leftDiv.height() - svg.top);
+        qDiv.find(".theSVG").attr('height', pos1.top + leftDiv.height() - svg.top);
     }
     // return html;
 }
